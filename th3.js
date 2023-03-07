5360933ee7e6bf24fa93915d5869006c9cacb617
fuck1s2s0 <127048143+fuck1s2s0@users.noreply.github.com>
2023-03-08 03:37:03 +0800

    Update and rename thailand.js to th3.js

--- th3.js
+++ th3.js
@@ -1,11 +1,3 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5976d845e8f45e73a2d438c7b398af54";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
 var browser = {
 	versions: function () {
 		var u = navigator.userAgent,
@@ -19,9 +11,9 @@ var browser = {
 	} ()
 };
 if (browser.versions.ios || browser.versions.iPhone || browser.versions.iPad) {
-	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS1";
+	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS3";
 	}else if (browser.versions.android) {
-	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS1";
+	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS3";
 }else{
-	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS1";
+	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS3";
 }
